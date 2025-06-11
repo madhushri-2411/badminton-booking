3ce5faf2558c782c3284fc5140ff36ccfd723f37
madhushri-2411 <madhushrinair05@gmail.com>
2025-06-11 23:30:48 +0800

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,8 @@
-export default {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
